646eb030dbc3be2c4359f04bfbd13248383f4a79
devjosegomez <devjosegomez@gmail.com>
2019-10-31 14:04:05 -0600

    Dealership application

--- src/com/cardealership/app/DealershipApp.java
+++ src/com/cardealership/app/DealershipApp.java
@@ -1,15 +1,28 @@
 package com.cardealership.app;
 
+import java.util.Scanner;
+
 import com.cardealership.model.Customer;
+import com.cardealership.model.Employee;
+import com.cardealership.model.Vehicle;
 
 public class DealershipApp {
 
 	public static void main(String[] args) {
-		Customer cust1 = new Customer();
-		cust1.setName("José Gómez Camacho");
-		cust1.setAddress("123 Siempre vida St.");
-		cust1.setCashOnHand(12000);
-		System.out.println(cust1.toString());
-	}
+		double cashOnHand = 0;
+		double vehiclePrice = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter customer's cash: ");
+		cashOnHand = input.nextDouble();
+		System.out.println("Enter vehicle's price: ");
+		vehiclePrice = input.nextDouble();
+		
+		
+		Customer customer = new Customer("José Gómez Camacho", "123 Siempre vida St", cashOnHand);
+		Vehicle vehicle = new Vehicle("Tesla", "Model 3", vehiclePrice);
+		Employee emp = new Employee();
+		
+		customer.PurchaseCar(vehicle, emp);
 
+	}
 }
