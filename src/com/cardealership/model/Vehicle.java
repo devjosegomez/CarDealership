593634d137fecb8ff0187673fd611d217a8694c3
devjosegomez <devjosegomez@gmail.com>
2019-10-31 11:38:02 -0600

    Added Vehicle's getters and setters

--- src/com/cardealership/model/Vehicle.java
+++ src/com/cardealership/model/Vehicle.java
@@ -1,5 +1,34 @@
 package com.cardealership.model;
 
 public class Vehicle {
+	private String make;
+	private String model;
+	private double price;
+	
+	//getters and setters
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getMake() {
+		return this.make;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
 	
 }
